326e60b391b90b321e6bf11630688aa434535e07
koloja <136496969+koloja@users.noreply.github.com>
2024-10-23 20:03:39 +0100

    really annoying me

--- src/classes/SkriptError.ts
+++ src/classes/SkriptError.ts
@@ -1,6 +1,4 @@
-export default class SkriptError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'SkriptError';
-    }
-}
+export default class SkriptError extends Error {constructor(message: string) {
+    super(message);
+    this.name = 'SkriptError';
+}};
